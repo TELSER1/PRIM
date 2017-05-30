d522008df292674f047368f4fbc4f49dab966fbe
Timothy Elser <timothyelser@Timothys-MBP.attlocal.net>
2017-05-29 19:19:30 -0700

    Need to fix way real values are being put into conditional statements

--- prim.py
+++ prim.py
@@ -4,18 +4,41 @@ import collections
 import pdb
 
 def condition_chainer(conditions):
-    
-    query_string="(" + string_condition(conditions[0]) + ")"
+    query_string="not ((" + string_condition(conditions[0]) + ")"
     for cond in conditions[1:]:
-        query_string+=" & (" + string_condition(cond) + ")"
-    
+        query_string+=" and (" + string_condition(cond) + ")"
+    query_string+=")"
     return query_string
+def condense_condition(conditions):
+    overall_bounds={}
+    for i in conditions:
+        if isinstance(i[1],list):
+            try:
+                if overall_bounds[i[2]]['min']>=i[1][0]:
+                    overall_bounds[i[2]]['min']=i[1][0]
+                if overall_bounds[i[2]]['max']>i[1][1]:
+                    overall_bounds[i[2]]['max']=i[1][1]
+            except:
+                overall_bounds[i[2]]={}
+                overall_bounds[i[2]]['min']=i[1][0]
+                overall_bounds[i[2]]['max']=i[1][1]
+        else:
+            try:
+                if i[1] not in overall_bounds[i[2]]:
+                    overall_bounds[i[2]].append(i[1])
+            except:
+                overall_bounds[i[2]]=[]
+                overall_bounds[i[2]].append(i[1])
+    
+    return(overall_bounds)
+
 def string_condition(condition_):
     '''Build query string'''
+    quantile_dict={"max":">=","min":"<"}
     if isinstance(condition_[1],list):
-        return condition_[2]+">="+str(condition_[1][0])+" & "+condition_[2]+"<"+str(condition_[1][1])
+        return condition_[2]+quantile_dict[condition_[1][1]]+np.str(condition_[1][0])
     else:
-        return condition_[2]+"=="+condition_[1]
+        return condition_[2]+"=="+"'"+condition_[1]+"'"
 def split_score(x,y,alpha,beta,i,classes=None):
     '''
     x: Feature vector
@@ -33,18 +56,23 @@ def split_score(x,y,alpha,beta,i,classes=None):
             if score_>max_val:
                 max_val=score_
                 max_cat=category
-            return [max_val,max_cat,i]
+        return [max_val,max_cat,i]
     else:
         min_split=x.quantile(alpha)
         max_split=x.quantile(1-alpha)
-        return [y[(x>=min_split) & (x<max_split)].mean(),[min_split,max_split],i]
+        min_split_score = y[(x<min_split)].mean()
+        max_split_score = y[(x >= max_split)].mean()
+        if min_split_score>=max_split_score:
+            return [min_split_score,[min_split,"min"],i]
+        else:
+            return [max_split_score,[max_split,'max'],i]
 def winning_condition(cand):
     '''Return best conditional split for list cand'''
     max_val=-np.inf
     max_feature=None
     for candidate in cand:
         if candidate[0]>max_val:
-            max_val=candidate
+            max_val=candidate[0]
             max_features=candidate
     return(max_features)
 class Box:
@@ -52,7 +80,7 @@ class Box:
         self.conditions=conditions
         self.mean=mean
 class PRIM:
-    def __init__(self,beta=5,alpha=.05,bottom_up=True):
+    def __init__(self,beta=25,alpha=.05,bottom_up=True):
         '''
         beta: Minimum number of samples required to partition to a smaller box.
         alpha: Threshold for candidate partitions
@@ -69,9 +97,13 @@ class PRIM:
         conditions=[]
         while support>self.beta:
             support=x_view.shape[0]
-            candidates=Parallel(n_jobs=-1)(delayed(split_score)(x_view[i],y_view,self.alpha,self.beta,i,self.classes_[i]) for i in x_view.keys())
+            candidates=[]
+            for i in x_view.keys():
+                candidates.append(split_score(x_view[i],y_view,self.alpha,self.beta,i,self.classes_[i]))
+#            candidates=Parallel(n_jobs=-1)(delayed(split_score)(x_view[i],y_view,self.alpha,self.beta,i,self.classes_[i]) for i in x_view.keys())
             winning_filter=winning_condition(candidates)
             conditions.append(winning_filter)
+            print(string_condition(winning_filter))
             x_view=x_view.query(string_condition(winning_filter))
             y_view=y_view[x_view.index]
         return(conditions)
@@ -89,12 +121,14 @@ class PRIM:
         support=x_view.shape[0]
         while support>self.beta:
             support=x_view.shape[0]
-            self.fit_box(x_view, y_view)
-            self.box_conditions.append(self.fit_box(x_view, y_view))
+            box_=self.fit_box(x_view, y_view)
             pdb.set_trace()
+            print(condense_condition(box_))
+            if len(box_)==0:
+                break
+            self.box_conditions.append(box_)
             x_view=x_view.query(condition_chainer(self.box_conditions[-1]))
             y_view=y_view[x_view.index]
-            
         return
     def predict(X,y):
         return
@@ -102,6 +136,8 @@ class PRIM:
 if __name__ == "__main__":
     import pandas as pd
     import numpy as np
-    data=pd.DataFrame({"A":np.random.randint(0,100,100),"B":np.random.randint(0,100,100),"C":[np.random.choice(["apples","bananas","oranges"]) for i in range(0,100)],"D":np.random.normal(size=100)})
+    data=pd.DataFrame({"A":np.random.randint(0,100,1000),"B":np.random.randint(0,100,1000),"C":[np.random.choice(["apples","bananas","oranges"]) for i in range(0,1000)],"D":np.random.normal(size=1000)})
+    data['D'][data['C']=='apples']=200
+    print(data['D'].max())
     RGR = PRIM()
     RGR.fit(data[['A','B','C']],data['D'])
